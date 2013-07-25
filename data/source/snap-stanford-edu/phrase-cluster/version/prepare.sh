ce1c4ca7de487408b22bce582a349c5363450094
Tim L <timrdf@gmail.com>
2013-07-25 13:28:22 -0400

    stub prepare trigger

--- data/source/snap-stanford-edu/phrase-cluster/version/prepare.sh
+++ data/source/snap-stanford-edu/phrase-cluster/version/prepare.sh
@@ -1,3 +1,3 @@
-#3> <> prov:specializationOf <> .
+#3> <> prov:specializationOf <https://github.com/difrad/sociam-web-observatory/blob/master/data/source/snap-stanford-edu/phrase-cluster/version/prepare.sh> .
 
 cat source/clust-qt08080902w3mfq5.txt | awk -f ../../src/shift-B-and-C-records.awk  > manual/clust-qt08080902w3mfq5.csv
