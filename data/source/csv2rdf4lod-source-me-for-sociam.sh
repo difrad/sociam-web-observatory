eadfa047ce9da5a8c701fad76929b945f38b7c04
Tim Lebo <timrdf@gmail.com>
2013-07-29 19:37:04 +0000

    no raw by default in project params

--- data/source/csv2rdf4lod-source-me-for-sociam.sh
+++ data/source/csv2rdf4lod-source-me-for-sociam.sh
@@ -201,8 +201,8 @@ export CSV2RDF4LOD_CONVERT_ALWAYS_UPDATE_CONVERTER="false"
 #
 # a conversion:ProjectLevelEnvironmentVariable
 # a conversion:ActivityLevelEnvironmentVariable
-export CSV2RDF4LOD_CONVERT_OMIT_RAW_LAYER="true"
 export CSV2RDF4LOD_CONVERT_OMIT_RAW_LAYER="false"
+export CSV2RDF4LOD_CONVERT_OMIT_RAW_LAYER="true"
 
 #
 # Customize: Number of rows to include in an example subset.
